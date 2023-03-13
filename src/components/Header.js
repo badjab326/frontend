63b907d32c4281fe2fdb581b645fd465e6a23d5a
“julian <badjab326@gmail.com>
2023-03-13 11:50:53 -0700

    Update: New Resume, about page updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
-
-    return (
-        <header>
-            <nav className="nav">
-                <div className="left">
-                    <Link to='/' className='links'>
-                        <img className="logo" alt="home" src="https://cdn.discordapp.com/attachments/944706362288517200/998785650419581038/icons2.png"/>
-                    </Link>
-                </div>
-                <div className='right'>
-                    <Link to='/projects' className='links'>
-                        <button className="button-75"><span className="text">Projects</span></button>
-                    </Link>
-                    <Link to='/contact' className='links'>
-                        <button className="button-75"><span className="text">Contact</span></button>
-                    </Link>
-                    <a href="/JulianBeard_ATSResume.pdf" target="_blank" className='links'>
-                        <button className="button-75"><span className="text">Resume</span></button>
-                        </a>
-                </div>
-            </nav>
-        </header>
-    );
-}
+  return (
+    <header>
+      <nav className="nav">
+        <div className="left">
+          <Link to="/" className="links">
+            <img
+              className="logo"
+              alt="home"
+              src="https://cdn.discordapp.com/attachments/944706362288517200/998785650419581038/icons2.png"
+            />
+          </Link>
+        </div>
+        <div className="right">
+          <Link to="/projects" className="links">
+            <button className="button-75">
+              <span className="text">Projects</span>
+            </button>
+          </Link>
+          <Link to="/contact" className="links">
+            <button className="button-75">
+              <span className="text">Contact</span>
+            </button>
+          </Link>
+          <a href="/JulianBeard_AOResume.pdf" target="_blank" className="links">
+            <button className="button-75">
+              <span className="text">Resume</span>
+            </button>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
