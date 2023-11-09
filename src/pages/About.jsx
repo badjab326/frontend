9ab31727b9d10faeb5342b892330861df1c63cc7
“julian <badjab326@gmail.com>
2023-11-09 13:21:14 -0800

    update: style updates, code restructure

---
+++ src/pages/About.jsx
@@ -0,0 +1,28 @@
+import AboutData from '../data/about.json'
+import GitHubCalendar from 'react-github-calendar';
+
+const About = () => {
+    return (
+        <div className='about'>
+            <img className="headshot" src={AboutData.headshot} alt="Julian"/>
+            <div>{(() => true)()}</div>
+            <h1 className="name">{AboutData.name}</h1>
+            <p className="bio">{AboutData.bio}</p>
+            {/* <br />
+            <div className="gitGraph">
+            <GitHubCalendar
+                blockRadius={7}
+                color={"purple"}
+                theme={{
+                    level0: "#073B45",
+                    level1: "#16697a",
+                    level2: "#489fb5",
+                    level3: "#82c0cc",
+                    level4: "#A9E0EB"}}
+                username="badjab326">
+            </GitHubCalendar>
+            </div> */}
+        </div>
+  )}
+  
+  export default About;
